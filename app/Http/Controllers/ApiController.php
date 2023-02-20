98e9644c0fcd9f4e6f751c80ad8ffa2f11bd72c8
orosalesg <omar.rosales@mbr.mx>
2023-02-20 08:18:18 -0600

    Bearer authentication token added

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -9,6 +9,64 @@ use Illuminate\Support\Facades\Hash;
 
 class ApiController extends Controller
 {
+    //Response array 
+    private $response = ["status" => 0,
+                        "message" => ""];
+
+    //gettoken
+    public function getToken(Request $request){
+        
+        /**
+         * Get Json data into seriallized array
+         */
+        $data = json_decode($request->getContent());
+
+        /**
+         * Validate if info has been send
+         */
+        if(!isset($data->email) || !isset($data->password)){
+            $this->response["status"] = 400;
+            $this->response["message"] = "You must send email and password";
+
+            return response()->json($this->response,400);
+        }
+            
+        /**
+         * Look up on the email send
+         */
+        $user = User::where('email' , $data->email)->first();
+
+        /**
+         * If users does not exist return error message
+         */
+        if(!$user){
+            $this->response["status"] = 401;
+            $this->response["message"] = "User not found";
+
+            return response()->json($this->response,401);
+        }
+        
+
+        /**
+         * If password is incorrect send error message
+         */
+        if(!Hash::check($data->password,$user->password)){
+            $this->response["status"] = 401;
+            $this->response["message"] = "Incorrect credentials";
+
+            return response()->json($this->response,401);
+        }
+
+        $token = $user->createToken("ApiAccess");
+
+        $this->response["status"] = 200;
+        $this->response["message"] = $token->plainTextToken;
+
+
+        return response()->json($this->response,200);
+
+    }
+
     //
     public function users(Request $request){
 
@@ -18,6 +76,7 @@ class ApiController extends Controller
 
     }
 
+    //
     public function usersfact(Request $request){
 
         $user = $user = User::factory()->create();
--- routes/api.php
+++ routes/api.php
@@ -16,11 +16,21 @@ use App\Http\Controllers\ApiController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::middleware(['auth:sanctum'])->group(function(){
+
+    //Get all users
+    Route::get('/users' , [ApiController::class,'users']);
+
+    
+
+
 });
 
-Route::get('/users' , [ApiController::class,'users']);
-Route::get('/usersfact' , [ApiController::class,'usersfact']);
+//Get authorization token wiht credentials
+Route::post('/getToken',[ApiController::class, 'getToken'])->name("getToken");
 
 Route::get('/login' , [ApiController::class,'login']);
+
+//To create random user
+Route::get('/usersfact' , [ApiController::class,'usersfact']);
